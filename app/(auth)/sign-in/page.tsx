f5af1724c57d922c0fc71daf930c57faae5540d9
sabakapanadze2 <skapanadze@companysage.com>
2025-10-21 11:15:24 +0400

    implement auth UI and logic

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+    } catch (e) {}
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Log In Your Account</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@jsmastery.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email name is required",
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "Email address is required",
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Signing In" : "Sign I"}
+        </Button>
+        <FooterLink
+          text="Dont't have an account?"
+          linkText="Create an account"
+          href="/sign-up"
+        />
+      </form>
+    </>
+  );
+};
+
+export default SignIn;
